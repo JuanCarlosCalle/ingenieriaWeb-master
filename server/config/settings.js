f1a503131a3d88fa83a178f45f87e88d32060322
JuanCarlos <Juan.ing.udea@gmail.com>
2017-10-20 18:33:19 -0500

    start

--- server/config/settings.js
+++ server/config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'ingWeb',
-  'database': 'mongodb://heroku_g3b6b252:puu78fbge7f5fsc73oo0ekvt3j@ds121345.mlab.com:21345/heroku_g3b6b252'
+  'database': 'mongodb://heroku_wp7j4h3l:gk960lhp9ngb949qljsv386loe@ds035806.mlab.com:35806/heroku_wp7j4h3l'
 };
