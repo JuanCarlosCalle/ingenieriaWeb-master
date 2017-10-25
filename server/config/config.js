6357834d8f200ec00ffa7195ea4f7dfcc5597488
JuanCarlosCalleGarcia <leodaza1995@gmail.com>
2017-10-25 17:55:54 -0500

    ng build

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const express = require('express');
 
 module.exports = function (app) {
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended : true}));
   app.use(logger('dev'));
