2bc0689d6ddfa33d95322edd9eda9290156f8081
JuanCarlosCalleGarcia <leodaza1995@gmail.com>
2017-10-25 18:06:26 -0500

    ng build

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ require('./endpoints/routes')(app);
 
 app.use(express.static(distDir));
 
+app.get("/",(req,res)=>{
+
+    return res.sendFile("./dist/index.html")
+})
+
 app.listen(app.get('port'), () => {
     console.log(`Servidor express escuchado en el puerto ${app.get('port')}`, distDir);
 });
