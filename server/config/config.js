ded18ff32e446950848b696b9abda3ffaea08274
JuanCarlosCalleGarcia <leodaza1995@gmail.com>
2017-10-25 18:14:14 -0500

    server delete

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,5 @@ module.exports = function (app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended : true}));
   app.use(logger('dev'));
-  app.set('port',  process.env.PORT || 3000);
+  app.set('port',  process.env.PORT || 3001);
 };
